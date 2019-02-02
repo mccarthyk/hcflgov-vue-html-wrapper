9a5e317f84c61d06689e09f64300dab7679ec847
Kevin McCarthy <mccarthyk@hcflgov.net>
2019-02-02 17:18:04 -0500

    vue init mccarthyk/webpack-simple .

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// import 'hc-html-wrapper/dist/static/js/manifest'
+// import 'hc-html-wrapper/dist/static/js/vendor'
+// import 'hc-html-wrapper/dist/static/js/app'
+
+import Vue from 'vue'
+import App from './App.vue'
+
+// import router from './router'
+// import store from './store'
+
+new Vue({
+  el: '#app',
+  // router,
+  // store,
+  render: h => h(App)
+})
